1183549e365a965ec55d1d7e67d9c32a47728275
Felipe Palazzo <palazzofelipe@gmail.com>
2017-05-10 23:34:22 -0300

    Update ESlint

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Text, AppRegistry } from 'react-native'
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
 const App = () => {
   return (
