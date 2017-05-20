6508fac0f2b269693e00f7f5a49cd67d71c218b7
Felipe Palazzo <palazzofelipe@gmail.com>
2017-05-20 18:03:47 -0300

    Add styles to album detail

--- src/components/album-detail.js
+++ src/components/album-detail.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import Card from './card'
 import CardSection from './card-section'
 
-const AlbumDetail = (props) =>
-  <Card>
-    <CardSection>
-      <Text>{props.config.title}</Text>
-    </CardSection>
-  </Card>
+const AlbumDetail = (props) => {
+  const {
+    title,
+    artist,
+    image,
+    thumbnail_image
+  } = props.config
+  const {
+    thumbnail,
+    headerContent,
+    headerText,
+    thumbnailContainer,
+    imageStyle
+  } = styles
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainer}>
+          <Image
+            style={thumbnail}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContent}>
+          <Text style={headerText}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
+    </Card>
+  )
+}
+
+const styles = StyleSheet.create({
+  headerContent: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  headerText: {
+    fontSize: 18
+  },
+  thumbnail: {
+    height: 50,
+    width: 50,
+  },
+  thumbnailContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+});
 
 export default AlbumDetail
